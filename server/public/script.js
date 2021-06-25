b2be3ce258f2a9abb8a8395d49274a87d6ba79b4
Matthew Bouc <boucgm@gmail.com>
2021-06-25 15:08:03 -0500

    POST object created.  getOperationButton working and inputing to object

--- server/public/script.js
+++ server/public/script.js
@@ -1,5 +1,38 @@
 $(document).ready(readyNow);
 
+let calculatorObject = {
+    inputOne: '0',
+    operationInput: '0',
+    inputTwo: '0',
+};
+
+
 function readyNow(){
     console.log('in JQ')
+    $('.operationButton').on('click', getOperationButton)
 }
+
+function getOperationButton(){
+    let operation = $(this).attr('id');
+    calculatorObject.operationInput = operation;
+    console.log(`operator selected:`, operation)
+    console.log(calculatorObject)
+}
+
+
+
+
+// Get input 1
+
+
+// Get input 2
+
+// Get = button
+
+// get C button
+
+// Create an object that all manth inputs get sent to
+
+//GET request - need this to happen on page load and on equal button
+
+//POST request
